9f5bf45b075f785b2b3df71f874ec932d961aae9
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-08-08 11:47:00 -0300

    Cleaned the code and improved the logic

--- main.c
+++ main.c
@@ -6,31 +6,16 @@ int main(void)
 {
     char input[MAX_CHARS];
     char x;
-    char aux;
-    // Data input
     while (scanf("%s", &input) != EOF)
     {
         for (x = 0; x < MAX_CHARS; x++)
         {
-            if (input[x] > 'W')
-                input[x] = input[x] - 2;
-            else if (input[x] > 'P')
-                input[x] = input[x] - 1;
-
+            if (input[x] > 'P')
+                input[x] = input[x] - 1 - (input[x] / 'X');
             if (input[x] >= 'A' && input[x] <= 'Z')
-            {
-                aux = input[x] - 'A';
-                aux = aux / 3;
-                printf("%d", aux + FIRST_NUMBER);
-            }
-            else if (input[x] == '1' || input[x] == '0')
-            {
+                printf("%d", ((input[x] - 'A') / 3) + FIRST_NUMBER);
+            else if (input[x] == '1' || input[x] == '0' || input[x] == '-')
                 printf("%c", input[x]);
-            }
-            else if (input[x] == '-')
-            {
-                printf("-");
-            }
             else
                 break;
         }
